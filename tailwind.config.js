7aa4314cdb61e7ff45245d680edb9b1e693276f7
Sean <sean@seanhay.com>
2022-05-14 01:22:54 +0900

    Convert to vite project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  content: ["./**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./index.html",
+		"./src/**/*.{vue,js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
